2bd88de62b6d735537fd823203e012b7dbeafdd5
amitp <amitpatel_7@msn.com>
2019-09-06 15:16:35 -0700

    changed fontawesome icon to faAngleUp

--- src/components/ScrollUpButton/index.js
+++ src/components/ScrollUpButton/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
 
 const ScrollUpButton = () => {
   return (
     <div className='email_button'>
       <a href='http://dataharvest.co/#'>
-        <FontAwesomeIcon icon={faArrowUp} className='fontawesome_icon' size='2x' />
+        <FontAwesomeIcon icon={faAngleUp} className='fontawesome_icon' size='2x' />
       </a>
     </div>
   )
