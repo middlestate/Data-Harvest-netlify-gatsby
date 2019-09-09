cbc3d2d4f4f1bd3b1b97d143206d8508a3183e9f
amitp <amitpatel_7@msn.com>
2019-09-09 03:43:42 -0700

    removed previous Project page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,7 +2,7 @@ import CMS from 'netlify-cms'
 import '../assets/sass/styles.sass'
 import HomePagePreview from './preview-templates/HomePagePreview'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
-import ProjectPagePreview from './preview-templates/ProjectPagePreview'
+// import ProjectPagePreview from './preview-templates/ProjectPagePreview'
 import ArticlePreview from './preview-templates/ArticlePreview'
 import PricingPagePreview from './preview-templates/PricingPagePreview'
 import ContactPagePreview from './preview-templates/ContactPagePreview'
@@ -10,7 +10,7 @@ import ContactPagePreview from './preview-templates/ContactPagePreview'
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('home', HomePagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('project', ProjectPagePreview)
+// CMS.registerPreviewTemplate('project', ProjectPagePreview)
 CMS.registerPreviewTemplate('pricing', PricingPagePreview)
 CMS.registerPreviewTemplate('contact', ContactPagePreview)
 CMS.registerPreviewTemplate('blog', ArticlePreview)
