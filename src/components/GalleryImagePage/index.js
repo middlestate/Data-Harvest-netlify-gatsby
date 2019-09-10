21e25520e68fe1cab6dd024c0e5db79b56cc7641
amitp <amitpatel_7@msn.com>
2019-09-10 15:14:10 -0700

    created new component GalleryImagePage

---
+++ src/components/GalleryImagePage/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const GalleryImagePage = () => (
+  <div className='gallery_image_page'>
+    <section className='hero section'>
+      <h1 className='project_title'>Project Title</h1>
+    </section>
+    <section className='project_description section'>
+      <p className='project_description_1'>Description Paragraph 1</p>
+      <p className='project_description_2'>Description Paragraph 2</p>
+      <p className='project_description_3'>Description Paragraph 3</p>
+    </section>
+  </div>
+)
+
+export default GalleryImagePage
+
