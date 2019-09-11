bb71d92f9816e74a49d798d12b1458d51ea97cd5
amitp <amitpatel_7@msn.com>
2019-09-10 22:46:25 -0700

    added preview for firstblock ledger in cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -7,6 +7,9 @@ import ArticlePreview from './preview-templates/ArticlePreview'
 import PricingPagePreview from './preview-templates/PricingPagePreview'
 import ContactPagePreview from './preview-templates/ContactPagePreview'
 
+/* Gallery Image Pages */
+import FirstBlockLedgerPagePreview from './preview-templates/FirstBlockLedgerPagePreview'
+
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('home', HomePagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
@@ -14,3 +17,5 @@ CMS.registerPreviewTemplate('project', ProjectPagePreview)
 CMS.registerPreviewTemplate('pricing', PricingPagePreview)
 CMS.registerPreviewTemplate('contact', ContactPagePreview)
 CMS.registerPreviewTemplate('blog', ArticlePreview)
+
+CMS.registerPreviewTemplate('firstblockledger', FirstBlockLedgerPagePreview)
