114e459eff0f052d490826bf4a108e243954add5
amitp <amitpatel_7@msn.com>
2019-09-11 21:54:49 -0700

    completed ScrollUpButton functionality

--- src/components/ScrollUpButton/index.js
+++ src/components/ScrollUpButton/index.js
@@ -5,7 +5,7 @@ import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
 const ScrollUpButton = () => {
   return (
     <div className='scroll_up_button'>
-      <a href='http://dataharvest.co/#'>
+      <a href='#'>
         <FontAwesomeIcon icon={faAngleUp} className='fontawesome_icon' size='2x' />
       </a>
     </div>
