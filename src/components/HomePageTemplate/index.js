6eb086e973866ff7efaeb74a3eaf89878a913eb7
amitp <amitpatel_7@msn.com>
2019-09-13 16:30:12 -0700

    copied video file to assets/img and adjusted import statement

--- src/components/HomePageTemplate/index.js
+++ src/components/HomePageTemplate/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {Link} from 'gatsby'
-import dataharvest_hero from '/static/img/dataharvest_hero.mp4'
+import dataharvest_hero from '../../assets/img/dataharvest_hero.mp4'
 
 const HomePageTemplate = ({
   header_title_image,
