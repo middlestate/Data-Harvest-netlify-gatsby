4bb90f7faa3da5996997053d51aaf28689978151
amitp <amitpatel_7@msn.com>
2019-09-13 18:16:24 -0700

    added z-index to .header_title_image and subheading

--- src/components/HomePageTemplate/index.js
+++ src/components/HomePageTemplate/index.js
@@ -22,13 +22,11 @@ const HomePageTemplate = ({
 }) => (
   <div>
     <section className='hero section'>
-      <div className='hero is-fullheight'>
-        <div className='hero-video'>
-          <div className='is-hidden-mobile'>
-            <video playsInline autoPlay muted loop poster=''>
-              <source src={dataharvest_hero} type='video/mp4' />
-            </video>
-          </div>
+      <div className='hero-video'>
+        <div className='is-hidden-mobile'>
+          <video playsInline autoPlay muted loop poster=''>
+            <source src={dataharvest_hero} type='video/mp4' />
+          </video>
         </div>
       </div>
       <div className='container'>
